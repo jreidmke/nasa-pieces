df5598288bf154ac11b1f7957d1534f5e6a53430
unknown <jreidmke@gmail.com>
2020-10-03 10:17:40 -0500

    init commit

---
+++ visable_satellites_call.py
@@ -0,0 +1,10 @@
+from flask import Flask
+import requests
+
+BASE_URL= "https://www.n2yo.com/rest/v1/satellite/"
+api_key="U9J35D-GT6QWZ-FPTKCN-4KD4" #don't abuse my lack of privacy team
+
+#/above/{observer_lat}/{observer_lng}/{observer_alt}/{search_radius}/{category_id}
+
+# ^^^^request format^^^^^^^^^^
+
