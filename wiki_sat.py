b4525ac7d85312dc5bf141456ad64c8e8d7bfc21
unknown <jreidmke@gmail.com>
2020-10-03 12:27:29 -0500

    parse_for_sat counts occurances of str atellite

--- wiki_sat.py
+++ wiki_sat.py
@@ -5,10 +5,24 @@ import requests
 
 BASE_URL = "https://en.wikipedia.org/w/api.php?action=parse&page="
 
-Magnum_(satellite)
+FORMAT_PARAMETERS_HTML = "&format=json&prop=text&formatversion=2"
+FORMAT_PARAMETERS_WIKITEXT = "&format=json&prop=wikitext&formatversion=2"
 
-FORMAT_PARAMETERS = "&format=json&prop=wikitext&formatversion=2"
+def call_html(search_term):
+    """Returns response in HTML format"""
 
-def call_wiki(search_term):
-    resp = requests.get(f"{BASE_URL}{search_term}{FORMAT_PARAMETERS}").json()
+    resp = requests.get(f"{BASE_URL}{search_term}{FORMAT_PARAMETERS_HTML}").json()["parse"]["text"]
     import pdb; pdb.set_trace()
+
+def call_wikitext(search_term):
+    """Returns response in WikiText format"""
+
+    resp = requests.get(f"{BASE_URL}{search_term}{FORMAT_PARAMETERS_WIKITEXT}").json()["parse"]["wikitext"]
+    parse_for_sat(search_term)
+
+def parse_for_sat(response, search_term):
+    sat_count = response.count('atellite')
+    if sat_count > 2:
+        return response
+    else:
+        call_wikitext(f'{search_term}' + '_(Satellite)')
